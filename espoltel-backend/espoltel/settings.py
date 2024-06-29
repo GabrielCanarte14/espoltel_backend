07641dcf8b481f3bad0fc141256f50dce393e66c
lgcanarte <lgcanarte@hubilogist.com>
2024-06-29 16:21:50 -0500

    procfile

--- espoltel-backend/espoltel/settings.py
+++ espoltel-backend/espoltel/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from datetime import timedelta
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2-=7=fg6&po!0266%95992wgu(ttorawr9%6-l_-)jsobju@ro'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ["*"]
 
