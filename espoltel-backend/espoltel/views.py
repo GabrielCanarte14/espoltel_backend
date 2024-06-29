014459fc56268cb2fc6fb6598a0c8ee7cc072ad9
lgcanarte <lgcanarte@hubilogist.com>
2024-06-29 17:58:10 -0500

    casi listo

--- espoltel-backend/espoltel/views.py
+++ espoltel-backend/espoltel/views.py
@@ -1,8 +1,6 @@
-from flask import Flask, render_template, request
+from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-
-from django.shortcuts import render
 
 def saludo(request):
     return HttpResponse("Hola, mundo!")
