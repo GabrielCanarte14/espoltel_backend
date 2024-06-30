c6abcafedfec63837f6eed4773f6fab80e218afb
lgcanarte <lgcanarte@hubilogist.com>
2024-06-29 19:34:18 -0500

    zs

--- espoltel/espoltel/settings.py
+++ espoltel/espoltel/settings.py
@@ -52,7 +52,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'espoltel.espoltel.api',
     'corsheaders',
     'proyecto',
     'user',
